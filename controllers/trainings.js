45ce0b02627d1de0e0284826109085f295004e5f
feherszabolcs <feherszabi2005@gmail.com>
2023-11-14 09:38:06 +0100

    :sparkles: Added morgan

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -3,24 +3,31 @@
 // @access Public
 exports.getTrainings = (req, res, next) => {
   res.status(200).json({ success: true, msg: 'Show all trainings' })
-} // @desc   Get single training
+}
+// @desc   Get single training
 // @route  GET /api/trainings/:id
 // @access Public
 exports.getTraining = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Get training ${req.params.id}` })
-} // @desc   Create new training
+  res.status(200).json({
+    success: true,
+    msg: `Get training ${req.params.id}`,
+  })
+}
+// @desc   Create new training
 // @route  POST /api/trainings
 // @access Private
 exports.createTraining = (req, res, next) => {
   res.status(200).json({ success: true, msg: 'Create new training' })
-} // @desc   Update training
+}
+// @desc   Update training
 // @route  PUT /api/trainings/:id
 // @access Private
 exports.updateTraining = (req, res, next) => {
   res
     .status(200)
     .json({ success: true, msg: `Update training ${req.params.id}` })
-} // @desc   Delete training
+}
+// @desc   Delete training
 // @route  DELETE /api/trainings/:id
 // @access Private
 exports.deleteTraining = (req, res, next) => {
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require("express");
-require("dotenv").config();
-const trainings = require("./routes/trainings");
+const express = require('express')
+require('dotenv').config()
+const trainings = require('./routes/trainings')
+const morgan = require('morgan')
 
-const app = express();
+const app = express()
 app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+  console.log(`Server running on port ${process.env.PORT}`),
+)
 
-app.use("/api/trainings", trainings);
+app.use(morgan('dev'))
+
+app.use('/api/trainings', trainings)
