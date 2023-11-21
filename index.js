028e8e2841fe5efca21b2f723b6c7901f3406f0f
feherszabolcs <feherszabi2005@gmail.com>
2023-11-21 08:32:29 +0100

    :sparkles: Added Training model

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 require('dotenv').config()
-const trainings = require('./routes/trainings')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const trainings = require('./routes/trainings')
 
 const app = express()
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`),
 )
+
+mongoose.set('strictQuery', true)
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString)
+const database = mongoose.connection
+
+database.on('error', (error) => {
+  console.log(error)
+})
+database.once('connected', () => {
+  console.log(`Database Connected ${database.host}`)
+})
 
 app.use(morgan('dev'))
 
