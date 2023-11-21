f15080e4f39362944168f598ef664a6daef9376b
feherszabolcs <feherszabi2005@gmail.com>
2023-11-21 09:31:40 +0100

    :zap: Added GET methods

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -2,17 +2,32 @@ const Training = require('../models/Training')
 // @desc   Get all trainings
 // @route  GET /api/trainings
 // @access Public
-exports.getTrainings = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all trainings' })
+exports.getTrainings = async (req, res, next) => {
+  try {
+    const trainings = await Training.find()
+    res.status(200).json({
+      success: true,
+      count: trainings.length,
+      data: trainings,
+    })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
 }
 // @desc   Get single training
 // @route  GET /api/trainings/:id
 // @access Public
-exports.getTraining = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Get training ${req.params.id}`,
-  })
+exports.getTraining = async (req, res, next) => {
+  try {
+    const training = await Training.findById(req.params.id)
+    if (!training) {
+      return res.status(400).json({ success: false, msg: 'Failed to find' })
+    }
+    return res.status(200).json({ success: true, data: training })
+  } catch (error) {
+    res.status(400).json({ success: false })
+    console.log(error)
+  }
 }
 // @desc   Create new training
 // @route  POST /api/trainings
