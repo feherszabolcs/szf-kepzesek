2661d6863433cd9d180487e33cb4c7936e9a7874
feherszabolcs <feherszabi2005@gmail.com>
2023-11-28 08:28:00 +0100

    :sparkles: training controller edited

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -43,16 +43,29 @@ exports.createTraining = async (req, res, next) => {
 // @desc   Update training
 // @route  PUT /api/trainings/:id
 // @access Private
-exports.updateTraining = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update training ${req.params.id}` })
+exports.updateTraining = async (req, res, next) => {
+  try {
+    const training = await Training.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!training)
+      return res.status(400).json({ success: false, msg: 'Not found!' })
+    res.status(200).json({ success: true, data: training })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
 }
 // @desc   Delete training
 // @route  DELETE /api/trainings/:id
 // @access Private
-exports.deleteTraining = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete training ${req.params.id}` })
+exports.deleteTraining = async (req, res, next) => {
+  try {
+    const training = await Training.findByIdAndDelete(req.params.id)
+    if (!training)
+      return res.status(400).json({ succes: false, msg: 'Not found!' })
+    res.status(200).json({ success: true, data: training })
+  } catch (error) {
+    res.status(400).json({ succes: false })
+  }
 }
